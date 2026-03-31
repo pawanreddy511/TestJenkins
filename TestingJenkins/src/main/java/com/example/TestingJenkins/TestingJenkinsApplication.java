346c1b7597340cf10ab38d6bc4309dec70c53134
pawanreddy511 <51431799+pawanreddy511@users.noreply.github.com>
2026-03-31 12:30:26 +0530

    first commit

---
+++ TestingJenkins/src/main/java/com/example/TestingJenkins/TestingJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestingJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingJenkinsApplication.class, args);
+	}
+
+}
---
+++ TestingJenkins/src/test/java/com/example/TestingJenkins/TestingJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestingJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
